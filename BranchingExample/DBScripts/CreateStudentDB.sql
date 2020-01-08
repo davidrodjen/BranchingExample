e2ba196054652a37f38ebe8078abc0081fe98c6c
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-01-08 14:24:46 -0800

    Created a student DB script

---
+++ BranchingExample/DBScripts/CreateStudentDB.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE Student
+GO
+
+USE Student
+GO
+
+CREATE TABLE Student
+(
+	FirstName		varchar(22) NOT NULL
+	,LastName		varchar(22) NOT NULL
+	,DateOfBirth	date		NULL
+	,CellPhone		char(10)	NULL
+)
